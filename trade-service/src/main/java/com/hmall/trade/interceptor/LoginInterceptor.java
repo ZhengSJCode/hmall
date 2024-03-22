ff16ac9e8ef74e492f27695bdda7001be23b8184
zhengshuaijie <352326430@qq.com>
2024-03-22 22:10:16 +0800

    微服务拆分

---
+++ trade-service/src/main/java/com/hmall/trade/interceptor/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.hmall.trade.interceptor;
+
+import com.hmall.common.utils.UserContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RequiredArgsConstructor
+public class LoginInterceptor implements HandlerInterceptor {
+
+//  TODO  private final JwtTool jwtTool;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.获取请求头中的 token
+        String token = request.getHeader("authorization");
+        // 2.校验token
+//    TODO    Long userId = jwtTool.parseToken(token);
+        // 3.存入上下文
+        UserContext.setUser(1L);
+        // 4.放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 清理用户
+        UserContext.removeUser();
+    }
+}
---
+++ user-service/src/main/java/com/hmall/user/UserApplication.java
@@ -0,0 +1,18 @@
+package com.hmall.user;
+
+import com.hmall.api.config.DefaultFeighConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients(basePackages = "com.hmall.api.client",defaultConfiguration = DefaultFeighConfig.class)
+@MapperScan("com.hmall.user.mapper")
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
